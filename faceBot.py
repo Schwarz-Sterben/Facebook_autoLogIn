c71fb5f152b35654b24e259e53bb830fd587d79b
Schwarz-Sterben <seanmiranda2@gmail.com>
2020-07-04 10:28:16 +0800

    added faceBot.py

---
+++ faceBot.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+username = ''
+password = ''
+
+class faceBot:
+	def __init__(self, username, password):
+		driver = webdriver.Firefox()
+		driver.get("https://www.facebook.com")
+		driver.get_elements_from_xPath('<input xPath here>').send_keys(username)
+		driver.get_elements_from_xPath('<input xPath here>').send_keys(password)
+		driver.send_keys(Keys.ENTER)
+		driver.close()
+	
+username = input("Enter Username: ")
+password = getpass("Enter password")
+a = faceBot(username, password)
+
+		
+		
+		
+		
+		
