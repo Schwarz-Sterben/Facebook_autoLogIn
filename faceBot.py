1ca592c3cc202ceec3b57f3e3f314cd7d08efbc8
Schwarz-Sterben <seanmiranda2@gmail.com>
2020-07-10 12:11:36 +0800

    code updated

--- faceBot.py
+++ faceBot.py
@@ -2,14 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from getpass import getpass
 import time
+import bcrypt
 
 class faceBot:
 	def __init__(self, username, password):
 		driver = webdriver.Firefox()
 		driver.get("https://www.facebook.com")
-		driver.get_elements_from_xPath('<input xPath here>').send_keys(username)
-		driver.get_elements_from_xPath('<input xPath here>').send_keys(password)
+		driver.get_elements_from_xPath("//input[contains(@type,'email')]").send_keys(username)
+		driver.get_elements_from_xPath("//input[@name='pass']").send_keys(password)
 		driver.send_keys(Keys.ENTER)
+	def hashing(self, password):
+		salt = bcrypt.gensalt()
+		hashed = bcrypt.hashpw(password, salt)
+		
 
 username = input("Enter Username: ")
 password = getpass("Enter password")
