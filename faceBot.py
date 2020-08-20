3fe248f6a8368dbbda8ba0d52a637caa538334dd
SchwarzSterben <seanmiranda2@gmail.com>
2020-08-20 10:02:17 +0800

    fixes

--- faceBot.py
+++ faceBot.py
@@ -10,12 +10,11 @@ class faceBot:
 	def __init__(self, username, password):
         username = mainWindow.username
         password = mainWindow.password
-	    driver = webdriver.Firefox()
-		driver.get("https://www.facebook.com")
-		driver.get_elements_from_xPath("//input[contains(@type,'email')]").send_keys(username)
-		driver.get_elements_from_xPath("//input[@name='pass']").send_keys(password)
-		driver.send_keys(Keys.ENTER)
-		
+        driver = webdriver.Firefox()
+        driver.get("https://www.facebook.com")
+        driver.get_elements_from_xPath("//input[contains(@type,'email')]").send_keys(username)
+        driver.get_elements_from_xPath("//input[@name='pass']").send_keys(password)
+        driver.send_keys(Keys.ENTER)
 
 username = input("Enter Username: ")
 password = getpass("Enter password")
