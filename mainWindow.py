a4de455c70b22ea69a4a6473010fd4e380e6ea7f
SchwarzSterben <seanmiranda2@gmail.com>
2020-08-20 10:08:50 +0800

    fixed

--- mainWindow.py
+++ mainWindow.py
@@ -1,23 +1,23 @@
 from tkinter import *
 import faceBot as fb
 #window
-tkWindow = Tk()  
-tkWindow.geometry('400x150')  
+tkWindow = Tk()
+tkWindow.geometry('400x150')
 tkWindow.title('Tkinter Login Form - pythonexamples.org')
 
 #username label and text entry box
 usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
 username = StringVar()
-usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)  
+usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)
 
 #password label and password entry box
-passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)  
+passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)
 password = StringVar()
-passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)  
+passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)
 
 
 
 #login button
-loginButton = Button(tkWindow, text="Login", command=fb.a).grid(row=4, column=0)  
+loginButton = Button(tkWindow, text="Login", command=fb.a).grid(row=4, column=0)
 
 tkWindow.mainloop()
